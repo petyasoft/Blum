464a80db644b5d6f3b54c7c1593528702de162f4
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-05-13 22:16:46 +0300

    Update register.py

--- utils/core/register.py
+++ utils/core/register.py
@@ -1,22 +1,67 @@
 from loguru import logger
 from data import config
 import pyrogram
-
+import os
+from data.config import USE_PROXY
 
 async def create_sessions():
     while True:
         session_name = input('Введите название сессии (для выхода нажмите Enter)\n')
         if not session_name:
             return
+        
+        directory = f'{os.path.dirname(__file__)}/sessions/'
+        len_sessions = len(os.listdir(directory))
+        
+        with open('proxy.txt','r') as file:
+            proxy = [i.strip() for i in file.readlines()]
+        
+        if USE_PROXY:
+            if len(proxy)>len_sessions:
+                
+                proxy = proxy[len_sessions]
+                
+                proxy_client = {
+                    "scheme": "socks5",
+                    "hostname": proxy.split(':')[0],
+                    "port": int(proxy.split(':')[1]),
+                    "username": proxy.split(':')[2],
+                    "password": proxy.split(':')[3],
+                }
+                session = pyrogram.Client(
+                    api_id=config.API_ID,
+                    api_hash=config.API_HASH,
+                    name=session_name,
+                    workdir=config.WORKDIR,
+                    proxy=proxy_client
+                )
 
-        session = pyrogram.Client(
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            name=session_name,
-            workdir=config.WORKDIR,
-        )
+                async with session:
+                    user_data = await session.get_me()
 
-        async with session:
-            user_data = await session.get_me()
+                logger.success(f'Добавлена сессия +{user_data.phone_number} @{user_data.username}')
+            
+            else:
+                session = pyrogram.Client(
+                    api_id=config.API_ID,
+                    api_hash=config.API_HASH,
+                    name=session_name,
+                    workdir=config.WORKDIR,
+                )
 
-        logger.success(f'Добавлена сессия +{user_data.phone_number} @{user_data.username}')
+                async with session:
+                    user_data = await session.get_me()
+
+                logger.success(f'Добавлена сессия +{user_data.phone_number} @{user_data.username}')
+        else:
+            session = pyrogram.Client(
+                api_id=config.API_ID,
+                api_hash=config.API_HASH,
+                name=session_name,
+                workdir=config.WORKDIR,
+            )
+
+            async with session:
+                user_data = await session.get_me()
+
+            logger.success(f'Добавлена сессия +{user_data.phone_number} @{user_data.username}')
