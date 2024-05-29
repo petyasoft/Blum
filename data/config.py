b147820dd8aff2f8efbb6a1109c7d910e849c94f
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-05-29 20:49:56 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -3,16 +3,16 @@ API_ID = 11111111
 API_HASH = 'Your api hash'
 
 # задержка между подключениями к аккаунтам
-ACC_DELAY = [1, 15]
+ACC_DELAY = [5, 15]
+
+# тип прокси
+PROXY_TYPE = "socks5" # http/socks5
 
 # папка с сессиями (не менять)
 WORKDIR = "sessions/"
 
 # использование прокси
 USE_PROXY = True # True/False
-
-# полный путь к вашей папке sessions 
-SESSIONS_PATH = "YOUR PATH TO SESSIONS"
 
 # скок поинтов с игры
 POINTS = [100, 140] #[min, max]
@@ -22,6 +22,9 @@ SPEND_DIAMONDS = True # True/False
 
 # сон между играми
 SLEEP_GAME_TIME = [100,200] #[min,max]
+
+# мини задержки
+MINI_SLEEP = [3,7]
 
 hello ='''              _                               __  _        
  _ __    ___ | |_  _   _   __ _  ___   ___   / _|| |_  ___ 
