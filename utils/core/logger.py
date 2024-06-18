cc99fd37dd87b5cf18e4a22a103fbd83ca3884ab
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-06-18 20:46:43 +0300

    Update logger.py

--- utils/core/logger.py
+++ utils/core/logger.py
@@ -6,8 +6,8 @@ def logging_setup():
     format_info = "<green>{time:HH:mm:ss.SS}</green> | <blue>{level}</blue> | <level>{message}</level>"
     logger.remove()
 
-    logger.add(sys.stdout, colorize=True,
-               format=format_info, level="INFO")
-
+    logger.add(sys.stdout, colorize=True, format=format_info, level="INFO")
+    
+    logger.add("blum_bot.log", rotation="50 MB", compression="zip", format=format_info, level="TRACE")
 
 logging_setup()
