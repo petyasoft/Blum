d394645e4da4142ba02c82d8e749b16fb307aeb1
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-10-30 01:29:20 +0300

    Update payload.py

--- utils/payload/payload.py
+++ utils/payload/payload.py
@@ -1,9 +1,18 @@
-import subprocess
 import asyncio
-
+import platform
+from data.config import NODE_PATH
 async def get_payload(gameId, points):
-    process = await asyncio.create_subprocess_exec('node', 'utils/payload/blum.mjs', gameId, str(points), stdout=asyncio.subprocess.PIPE)
+
+    is_linux = platform.system().lower() == 'linux'
+
+    if is_linux:
+        process = await asyncio.create_subprocess_exec('which', 'node', stdout=asyncio.subprocess.PIPE)
+        output, _ = await process.communicate()
+        node_path = output.decode('utf-8').strip()
+    else:
+        node_path = 'node'
+
+    process = await asyncio.create_subprocess_exec(NODE_PATH, 'utils/payload/blum.mjs', gameId, str(points), stdout=asyncio.subprocess.PIPE)
     output, _ = await process.communicate()
     payload = output.decode('utf-8').strip()
     return payload
-
