e6d32f589a49cc3e29fe6f0b3cfd8b63a61915a7
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-11-05 21:58:56 +0300

    Delete utils/payload/payload.py

--- utils/payload/payload.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import platform
-from data.config import NODE_PATH
-
-async def get_payload(gameId, points):
-    process = await asyncio.create_subprocess_exec(NODE_PATH, 'utils/payload/blum.mjs', gameId, str(points), stdout=asyncio.subprocess.PIPE)
-    output, _ = await process.communicate()
-    payload = output.decode('utf-8').strip()
-    return payload
